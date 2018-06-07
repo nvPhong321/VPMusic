512e5f083ff5edb42cd1956194ed3fd224d7a178
Nguyen Van Phong <nguyenphong.pn714@gmail.com>
2018-06-07 20:40:18 +0700

    first commit

---
+++ app/src/main/java/com/example/phong/v0h0xmusic/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.phong.v0h0xmusic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
